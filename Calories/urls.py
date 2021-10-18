10102692e552146bd5899cc9bc357f5886f7262a
Christophe Néraud <rubytox@rubytox.fr>
2021-10-18 22:00:50 +0000

    Crappy timezone version

--- Calories/urls.py
+++ Calories/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('calories_app.urls')),
     path('admin/', admin.site.urls),
+    path('tz_detect/', include('tz_detect.urls')),
 ]
